38327616cd7bff30fc6c346626f78bd80447f0f6
maerdlina <maerdlina@gmail.com>
2025-09-19 11:37:56 +0300

    init project

---
+++ src/main/java/ru/petprject/filedistributor/FileDistributorApplication.java
@@ -0,0 +1,13 @@
+package ru.petprject.filedistributor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDistributorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileDistributorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/petprject/filedistributor/FileDistributorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.petprject.filedistributor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDistributorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
